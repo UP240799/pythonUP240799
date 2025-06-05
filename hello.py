2d886866028978bbd00343edd41e2964346814c8
UP240799 <up240799@alumnos.upa.edu.mx>
2025-06-05 10:54:07 -0600

    creacion de primer archivo para repositorio

---
+++ hello.py
@@ -0,0 +1,36 @@
+import math
+
+print(3+2)
+print(3-2)
+print(3*2)
+print(3/2)
+print(3**2)
+print(3%2)
+print(3//2)
+
+print('hiram')
+print('iann')
+print('Mexico')
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type('Asabeneh', 'Python', 'Finland'))
+print(type('Hiram'))
+print(type('Iann'))
+print(type('Mexico'))
+
+print(type(5))
+print(type(2.5))
+print(type(5+8j))
+print(type('Omar'))
+print(type(4 == 4))
+print(type([2,5,7]))
+print(type((5.3,7.8,2.7)))
+print(type({'name':'Omar'}))
+
+point1=(2,3)
+point2=(10,8)
+distance= math.sqrt((point2[0]-point1[0])**2+(point2[1]-point1[1])**2)
+print(distance)
